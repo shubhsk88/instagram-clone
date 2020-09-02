edb53c03ad32179798ee79bc19d097b53893599d
Shubham Singh <shubham88singh@gmail.com>
2020-09-03 04:08:16 +0530

    Adding the mongoose not working

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,6 +1,6 @@
 import { ApolloServer, gql } from 'apollo-server-micro';
 import mongoose from 'mongoose';
-
+import connectDB from '../../lib/mongoose';
 const typeDefs = gql`
   type Query {
     me: String!
@@ -22,4 +22,4 @@ export const config = {
 };
 
 const server = apolloServer.createHandler({ path: '/api/graphql' });
-export default server;
+export default conectDB(server);
